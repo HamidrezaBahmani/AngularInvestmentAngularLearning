03b81abacc93450e0e97a3b77e66f8821c975823
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2025-07-04 13:42:37 +0330

    Using Service and Signals For project

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { InvestmentService } from '../investment.service';
 @Component({
   selector: 'app-user-input',
   standalone: true,
@@ -9,16 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
-
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('2');
+  enteredDuration = signal('10');
+  constructor(private investmentService: InvestmentService) {}
   onSubmit() {
-    console.log('sub');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.investmentService.CalculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+
+    this.enteredAnnualInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('0');
+    this.enteredDuration.set('10');
   }
 }
